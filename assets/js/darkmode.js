c1d6fda5e9cebb4fcd6a06ce9852c98e29314b31
Aleksandr Petrov <@am.petrov>
2022-07-12 19:38:24 +0300

    Move dark mode to main.js and add it to all pages

--- assets/js/darkmode.js
+++
@@ -1,4 +0,0 @@
-var dark_mode = document.getElementById("dark_mode_icon");
-dark_mode.onclick = function(){
-    document.body.classList.toggle("dark-mode");
-}
--- assets/js/main.js
+++ assets/js/main.js
@@ -333,3 +333,11 @@
 			});
 
 })(jQuery);
+
+
+/*  Dark mode logic  */
+
+var dark_mode = document.getElementById("dark_mode_icon");
+dark_mode.onclick = function(){
+    document.body.classList.toggle("dark-mode");
+};
